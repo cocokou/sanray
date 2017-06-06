c522b58b6fa9ab32ceb8519a94f9daa3bb1374cd
cocokou <isco@163.com>
2017-06-06 15:25:37 +0800

    copy bear to sanray

---
+++ src/js/redux/config/auth.config.js
@@ -0,0 +1,19 @@
+export default {
+  auth: {
+    "visitor": ["test_qrcode", "testAccess"],
+    'productmanager': [
+      'product_management',
+      'ProductManageAccess',
+      'ProductMapAccess',
+      'manu_management',
+      'ProcessManageAccess',
+      'auth_management',
+      'UserManageAccess',
+      'RoleAuthManageAccess',
+      'DeptRoleManageAccess',
+      'SysAuthManageAccess',
+      'test_qrcode',
+      'testAccess'
+    ],
+  }
+}
